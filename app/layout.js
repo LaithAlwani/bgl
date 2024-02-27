72bc91292f16a2fcb767ecaeb492fe59d64ff6d7
laith <laithalwani@gmail.com>
2024-02-27 16:31:41 -0500

    intial commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaSquareYoutube, FaXTwitter } from "react-icons/fa6";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +15,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <Toaster />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
+
+const Footer = () => {
+  const size = 24;
+  return (
+    <footer>
+      <div className="social-links">
+        <a href="#" aria-label="Facebook page">
+          <FaFacebookF size={size} />
+        </a>
+        <a href="#">
+          <FaInstagram size={size} aria-label="Instagram page" />
+        </a>
+        <a href="#">
+          <FaSquareYoutube size={size} aria-label="Linkedin page" />
+        </a>
+        <a href="#">
+          <FaXTwitter size={size} aria-label="X page" />
+        </a>
+      </div>
+
+      <span className="copy-right">&copy;2024 Ottawa, ON.</span>
+    </footer>
+  );
+};
---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="logo">
+        {/* <Link href="/">BGL</Link> */}
+        <h3>BGL</h3>
+      </div>
+      <div className="nav-links">
+        <Link href="/leagues">Leagues</Link>
+        <Link href="/signup">Sign Up</Link>
+        <Link href="/login">Login</Link>
+        <Link href="/logout">Logout</Link>
+      </div>
+    </nav>
+  );
+}
