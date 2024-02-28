15aa247c1d1856156bbe843591dc8f8476ed8781
laith <laithalwani@gmail.com>
2024-02-27 23:52:29 -0500

    adds sigin and sign out

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,37 @@
+"use client";
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { useUser } from "@clerk/nextjs";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { isSignedIn, user } = useUser();
+  
   return (
     <nav>
-      <div className="logo">
-        {/* <Link href="/">BGL</Link> */}
-        <h3>BGL</h3>
+      <div
+        className={`mobile-nav-btn ${isMenuOpen ? "open" : ""}`}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <div className="burger-icon"></div>
       </div>
-      <div className="nav-links">
-        <Link href="/leagues">Leagues</Link>
-        <Link href="/signup">Sign Up</Link>
-        <Link href="/login">Login</Link>
-        <Link href="/logout">Logout</Link>
-        <Link href="/leagues">Profile</Link>
+      <div className={`sidebar ${isMenuOpen ? "open" : ""}`}>
+        <div className="nav-links">
+          <Link href="/leagues">Leagues</Link>
+          {!isSignedIn ? (
+            <SignInButton className="btn" />
+          ) : (
+            <>
+              <Link href="/profile" className="profile">
+              {user.username} <img src={user.imageUrl} className="avatar" /> 
+              </Link>
+              <SignOutButton className=" btn btn-primary" />
+            </>
+          )}
+        </div>
       </div>
+      <Link href="/">BGL</Link>
+      {/* <h3>BGL</h3> */}
     </nav>
   );
 }
