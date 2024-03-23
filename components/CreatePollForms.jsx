05b2bb1de3fb5086e090090837621254bd13d9d9
laith <laithalwani@gmail.com>
2024-03-23 16:31:25 -0400

    adds votes to items

--- components/CreatePollForms.jsx
+++ components/CreatePollForms.jsx
@@ -1,12 +1,13 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
 export default function CreatePollForms() {
-  const [inputs, setInputs] = useState({});
+  const [title, setTitle] = useState('')
+  const [inputs, setInputs] = useState([]);
   const [itemNumbers, setItemNumbers] = useState(1);
-  const router = useRouter()
+  const router = useRouter();
 
   const handleClick = () => {
     setItemNumbers((prevState) => prevState + 1);
@@ -15,15 +16,16 @@ export default function CreatePollForms() {
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
+    setInputs(values => ({...values, [name]: value, votes:0}))
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputs);
-    toast.success(`poll ${inputs.title} created`);
+    toast.success(`poll ${title} created`);
+    setTitle('');
     setInputs({});
-    router.push('/admin/dashboard')
+    router.push("/admin/dashboard");
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -31,8 +33,8 @@ export default function CreatePollForms() {
         name="title"
         type="text"
         placeholder="Poll Title"
-        value={inputs.title || ""}
-        onChange={handleChange}
+        value={title}
+        onChange={(e)=>setTitle(e.target.value)}
       />
       <div>
         {Array.from(Array(itemNumbers), (e, i) => (
