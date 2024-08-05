db77ff78d63e903f37c5d572d521d69bdd14638e
laith <laithalwani@gmail.com>
2024-08-05 11:11:10 -0400

    adds loader to boardgames and boardgame pages

--- app/boardgames/[id]/page.js
+++ app/boardgames/[id]/page.js
@@ -1,33 +1,44 @@
 "use client";
+import Loader from "@/components/Loader";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function BoardgamePage() {
   const params = useParams();
   const [boardgame, setBoardgame] = useState({});
+  const [loading, setLoding] = useState(false);
+
   const getBoardgame = async () => {
+    setLoding(true);
     const res = await fetch(`/api/boardgames/${params.id}`);
     if (res.ok) {
       const data = await res.json();
       setBoardgame(data);
     }
+    setLoding(false);
   };
+
   useEffect(() => {
     getBoardgame();
   }, []);
+
   return (
-    <div className="page">
-      {boardgame && (
-        <>
-          <img src={boardgame.thumbnail} />
-          <h2>{boardgame.title}</h2>
-          <p>
-            {boardgame.minPlayers} - {boardgame.maxPlayers}
-          </p>
-          <p>
-            {boardgame.minPlayTime} - {boardgame.maxPlayTime}
-          </p>
-        </>
+    <div className="page flex-page">
+      {loading ? (
+        <Loader size={128} />
+      ) : (
+        boardgame && (
+          <>
+            <img src={boardgame.thumbnail} />
+            <h2>{boardgame.title}</h2>
+            <p>
+              {boardgame.minPlayers} - {boardgame.maxPlayers}
+            </p>
+            <p>
+              {boardgame.minPlayTime} - {boardgame.maxPlayTime}
+            </p>
+          </>
+        )
       )}
     </div>
   );
--- app/boardgames/page.js
+++ app/boardgames/page.js
@@ -1,30 +1,40 @@
 "use client";
+import Loader from "@/components/Loader";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function BoardgamesPage() {
   const [boardgames, setBoardgames] = useState([]);
+  const [loading, setLoding] = useState(false);
 
   const getBoardgames = async () => {
+    setLoding(true);
     setBoardgames([]);
     const res = await fetch("/api/boardgames");
     if (res.ok) {
       const data = await res.json();
       setBoardgames(data);
     }
+    setLoding(false);
   };
-  
+
   useEffect(() => {
     getBoardgames();
   }, []);
 
   return (
-    <section className="page">
-      {boardgames.map((bg) => (
-        <Link href={`/boardgames/${bg._id}`} key={bg._id} >
-          <img src={bg.thumbnail}  />
-        </Link>
-      ))}
+    <section className="flex-page page">
+      <div className="boardgame-container">
+        {!loading ? (
+          boardgames.map((bg) => (
+            <Link href={`/boardgames/${bg._id}`} key={bg._id}>
+              <img src={bg.thumbnail} />
+            </Link>
+          ))
+        ) : (
+          <Loader size={128} />
+        )}
+      </div>
     </section>
   );
 }
