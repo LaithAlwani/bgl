16579a098a8108bb790ddb24713c36dc964bcfbe
laith <laithalwani@gmail.com>
2024-08-06 23:54:45 -0400

    end of day

--- app/boardgames/[id]/page.js
+++ app/boardgames/[id]/page.js
@@ -3,11 +3,13 @@ import Loader from "@/components/Loader";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import "@/styles/boardgames.css";
+import "@/styles/contact.css";
 import toast from "react-hot-toast";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 export default function BoardgamePage() {
   const params = useParams();
-  const [boardgame, setBoardgame] = useState({});
+  const [boardgame, setBoardgame] = useState(null);
   const [loading, setLoding] = useState(false);
   const [modleToggle, setModleToggle] = useState(false);
 
@@ -30,34 +32,58 @@ export default function BoardgamePage() {
       {loading ? (
         <Loader size={128} />
       ) : (
-        boardgame && (
-          <>
-            <img src={boardgame.thumbnail} />
-            <h2>{boardgame.title}</h2>
-            <p>
-              {boardgame.minPlayers} - {boardgame.maxPlayers}
-            </p>
-            <p>
-              {boardgame.minPlayTime} - {boardgame.maxPlayTime}
-            </p>
-          </>
-        )
+        <>
+          <BoardGame
+            boardgame={boardgame}
+            modleToggle={modleToggle}
+            setModleToggle={setModleToggle}
+          />
+          <GamePlays sessions={boardgame?.sessions} />
+          <Modle modleToggle={modleToggle} setModleToggle={setModleToggle}>
+            <LogPlay
+              boardgame={boardgame}
+              setBoardgame={setBoardgame}
+              setModleToggle={setModleToggle}
+            />
+          </Modle>
+        </>
       )}
-      <Modle modleToggle={modleToggle} setModleToggle={setModleToggle}>
-        <LogPlay boardgame={boardgame} setBoardgame={setBoardgame} setModleToggle={setModleToggle} />
-      </Modle>
-      <button onClick={(e) => setModleToggle(!modleToggle)}>Log Play</button>
-      {boardgame.sessions?.length > 0 &&
-        boardgame.sessions.map((session) =>
-          session.players.map((player,i) => (
-            <p key={i}>
-              {player.player} points:{player.points}
-            </p>
-          ))
-        )}
     </div>
   );
 }
+
+const BoardGame = ({ boardgame, modleToggle, setModleToggle }) => {
+  return (
+    boardgame && (
+      <div className="boardgame">
+        <img src={boardgame.thumbnail} />
+        <div>
+          <h2>{boardgame.title}</h2>
+          <p>
+            Players: {boardgame.minPlayers} - {boardgame.maxPlayers}
+          </p>
+          <p>
+            Time: {boardgame.minPlayTime} - {boardgame.maxPlayTime}
+          </p>
+          <button className="btn btn-green" onClick={(e) => setModleToggle(!modleToggle)}>Log Play</button>
+        </div>
+      </div>
+    )
+  );
+};
+
+const GamePlays = ({ sessions }) => {
+  return (
+    sessions?.length > 0 &&
+    sessions.map((session) =>
+      session.players.map((player, i) => (
+        <p key={i}>
+          {player.player} points:{player.points}
+        </p>
+      ))
+    )
+  );
+};
 
 const LogPlay = ({ boardgame, setBoardgame, setModleToggle }) => {
   const [inputFields, setInputFields] = useState([
@@ -93,40 +119,46 @@ const LogPlay = ({ boardgame, setBoardgame, setModleToggle }) => {
     if (res.ok) {
       const data = await res.json();
       toast.success(data.message);
-      const fields = [{ player: "", points: 0 }]
-      const newSessions = boardgame.sessions
+      const fields = [{ player: "", points: 0 }];
+      const newSessions = boardgame.sessions;
       newSessions.push(data.session);
-      setBoardgame(prev=> prev, {sessions:newSessions})
-      setInputFields(fields)
+      setBoardgame((prev) => prev, { sessions: newSessions });
+      setInputFields(fields);
       setModleToggle(false);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>log play</h2>
+      <h2 className="text-3xl font-bold">log play</h2>
       {inputFields.map((input, idx) => (
-        <div key={idx}>
+        <div key={idx} style={{ display: "flex", gap: "0.5rem", alignItems: "center" }}>
           <input
             type="text"
             name={`player`}
             onChange={(e) => handleChange(e, idx)}
             placeholder={`player ${idx + 1}`}
             value={input.player}
+            style={{ flexBasis: "1 0 0" }}
           />
           <input
-            type="text"
+            type="number"
             name={`points`}
             onChange={(e) => handleChange(e, idx)}
             value={input.points}
-            placeholder={`points`}
+            min={0}
+            placeholder="score"
+            autoComplete="false"
+            style={{ flexBasis: "content" }}
           />
-          <span onClick={() => removePlayer(idx)}>X</span>
+          <MdOutlineDeleteOutline onClick={() => removePlayer(idx)} color="#b8373c" size={64} />
         </div>
       ))}
-      <span onClick={addPlayer}>Add</span>
+      <button type="button" onClick={addPlayer} className="btn">
+        Add
+      </button>
 
-      <button>Submit</button>
+      <button className="btn btn-green">Submit</button>
     </form>
   );
 };
